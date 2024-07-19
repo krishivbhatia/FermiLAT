b3bd84b6576f95f115c7d3671271b58df013b29f
Krishiv Bhatia <flyingunicorns5724@gmail.com>
2024-07-19 02:06:18 -0700

    hoined 3FGL and 4FGL

--- Python/Reporting/4FGL_Join_Report.py
+++ Python/Reporting/4FGL_Join_Report.py
@@ -113,7 +113,7 @@ for key in list(point_source_catalogue.header.keys()):
     if "TTYPE" in key:
         keymap[int(key[5:len(key)])-1] = point_source_catalogue.header[key]
         mapkey[point_source_catalogue.header[key]] = int(key[5:len(key)])-1
-        print(columns, key, point_source_catalogue.header[key])
+        # print(columns, key, point_source_catalogue.header[key])
         columns += 1
 
 # TTYPE65 0FGL_Name, TTYPE66 1FGL_Name, TTYPE67 2FGL_Name
@@ -122,6 +122,91 @@ dataset = FGL4Dataset_Process(point_source_catalogue, wanted_type_col, [64,65,66
 fgl3_lst = dataset.get_all_list()
 fgl3_df = pd.DataFrame(fgl3_lst, columns = ["3FGL", "CLASS1-3FGL", "3FGL-BSL", "3FGL-1FGL", "3FGL-2FGL"])
 fgl3_df.to_csv('../../CSV/fgl3_df.csv')
+fgl3_join_4fgl_df = fgl3_df[["3FGL", "CLASS1-3FGL"]]
+
+print("=====================4FGL-DR1=======================")
+columns = 0
+keymap = {}
+mapkey = {}
+path = os.path.join(os.getcwd(), "../../FITS/gll_psc_v22_4FGL_DR1.fit")
+mainfile = fits.open(path)
+point_source_catalogue = mainfile[1]
+for key in list(point_source_catalogue.header.keys()):
+    if "TTYPE" in key:
+        keymap[int(key[5:len(key)])-1] = point_source_catalogue.header[key]
+        mapkey[point_source_catalogue.header[key]] = int(key[5:len(key)])-1
+        # print(columns, key, point_source_catalogue.header[key])
+        columns += 1
+
+# TTYPE65 0FGL_Name, TTYPE66 1FGL_Name, TTYPE67 2FGL_Name
+wanted_type_col = 74 # class1
+dataset = FGL4Dataset_Process(point_source_catalogue, wanted_type_col, [67])
+fgl4dr1_lst = dataset.get_all_list()
+fgl4dr1_df = pd.DataFrame(fgl4dr1_lst, columns = ["4FGLDR1",  "CLASS1-4FGLDR1", "4FGLDR1-3FGL"])
+fgl4dr1_df.to_csv('../../CSV/fgl4dr1_df.csv')
+
+print("=====================4FGL-DR2=======================")
+columns = 0
+keymap = {}
+mapkey = {}
+path = os.path.join(os.getcwd(), "../../FITS/gll_psc_v27_4FGL_DR2.fit")
+mainfile = fits.open(path)
+point_source_catalogue = mainfile[1]
+for key in list(point_source_catalogue.header.keys()):
+    if "TTYPE" in key:
+        keymap[int(key[5:len(key)])-1] = point_source_catalogue.header[key]
+        mapkey[point_source_catalogue.header[key]] = int(key[5:len(key)])-1
+        # print(columns, key, point_source_catalogue.header[key])
+        columns += 1
+
+# TTYPE65 0FGL_Name, TTYPE66 1FGL_Name, TTYPE67 2FGL_Name
+wanted_type_col = 64 # class1
+dataset = FGL4Dataset_Process(point_source_catalogue, wanted_type_col, [57])
+fgl4dr2_lst = dataset.get_all_list()
+fgl4dr2_df = pd.DataFrame(fgl4dr2_lst, columns = ["4FGLDR2",  "CLASS1-4FGLDR2", "4FGLDR2-3FGL"])
+fgl4dr2_df.to_csv('../../CSV/fgl4dr2_df.csv')
+
+print("=====================4FGL-DR3=======================")
+columns = 0
+keymap = {}
+mapkey = {}
+path = os.path.join(os.getcwd(), "../../FITS/gll_psc_v31_4FGL_DR3.fit")
+mainfile = fits.open(path)
+point_source_catalogue = mainfile[1]
+for key in list(point_source_catalogue.header.keys()):
+    if "TTYPE" in key:
+        keymap[int(key[5:len(key)])-1] = point_source_catalogue.header[key]
+        mapkey[point_source_catalogue.header[key]] = int(key[5:len(key)])-1
+        print(columns, key, point_source_catalogue.header[key])
+        columns += 1
+
+# TTYPE65 0FGL_Name, TTYPE66 1FGL_Name, TTYPE67 2FGL_Name
+wanted_type_col = 69 # class1
+dataset = FGL4Dataset_Process(point_source_catalogue, wanted_type_col, [62])
+fgl4dr3_lst = dataset.get_all_list()
+fgl4dr3_df = pd.DataFrame(fgl4dr3_lst, columns = ["4FGLDR3",  "CLASS1-4FGLDR3", "4FGLDR3-3FGL"])
+fgl4dr3_df.to_csv('../../CSV/fgl4dr3_df.csv')
+
+print("=====================4FGL-DR4=======================")
+columns = 0
+keymap = {}
+mapkey = {}
+path = os.path.join(os.getcwd(), "../../FITS/gll_psc_v33_4FGL_DR4.fit")
+mainfile = fits.open(path)
+point_source_catalogue = mainfile[1]
+for key in list(point_source_catalogue.header.keys()):
+    if "TTYPE" in key:
+        keymap[int(key[5:len(key)])-1] = point_source_catalogue.header[key]
+        mapkey[point_source_catalogue.header[key]] = int(key[5:len(key)])-1
+        print(columns, key, point_source_catalogue.header[key])
+        columns += 1
+
+# TTYPE65 0FGL_Name, TTYPE66 1FGL_Name, TTYPE67 2FGL_Name
+wanted_type_col = 69 # class1
+dataset = FGL4Dataset_Process(point_source_catalogue, wanted_type_col, [62])
+fgl4dr4_lst = dataset.get_all_list()
+fgl4dr4_df = pd.DataFrame(fgl4dr4_lst, columns = ["4FGLDR4",  "CLASS1-4FGLDR4", "4FGLDR4-3FGL"])
+fgl4dr4_df.to_csv('../../CSV/fgl4dr4_df.csv')
 
 print("=========================Merge-BSL-1FGL=========================")
 joined_BSL_1FGL = pd.merge(bsl_df, fgl1_df, left_on='BSL', right_on='1FGL-BSL', how='left')
@@ -140,3 +225,42 @@ joined_BSL_1FGL_2FGL_3FGL = pd.merge(joined_BSL_1FGL_2FGL, fgl3_df, left_on='BSL
 joined_BSL_1FGL_2FGL_3FGL.to_csv('../../CSV/joined_BSL_1FGL_2FGL_3FGL.csv')
 joined_BSL_1FGL_2FGL_3FGL_NONE_CLS1_BSL = joined_BSL_1FGL_2FGL_3FGL[joined_BSL_1FGL_2FGL_3FGL["CLASS1-BSL"]=='']
 joined_BSL_1FGL_2FGL_3FGL_NONE_CLS1_BSL.to_csv('../../CSV/joined_BSL_1FGL_2FGL_3FGL_NONE_CLS1_BSL.csv')
+
+print("=========================Merge 3FGL, 4FGL-DR1=========================")
+joined_3FGL_4FGLDR1 = pd.merge(fgl3_join_4fgl_df, fgl4dr1_df, left_on='3FGL', right_on='4FGLDR1-3FGL', how='left')
+joined_3FGL_4FGLDR1.to_csv('../../CSV/joined_3FGL_4FGLDR1.csv')
+
+print("=========================Merge 3FGL, 4FGL-DR1, 4FGL-DR2=========================")
+joined_3FGL_4FGLDR1_DR2 = pd.merge(joined_3FGL_4FGLDR1, fgl4dr2_df, left_on='3FGL', right_on='4FGLDR2-3FGL', how='left')
+joined_3FGL_4FGLDR1_DR2.to_csv('../../CSV/joined_3FGL_4FGLDR1_DR2.csv')
+
+print("=========================Merge 3FGL, 4FGL-DR1, 4FGL-DR2, 4FGL-DR3=========================")
+joined_3FGL_4FGLDR1_DR2_DR3 = pd.merge(joined_3FGL_4FGLDR1_DR2, fgl4dr3_df, left_on='3FGL', right_on='4FGLDR3-3FGL', how='left')
+joined_3FGL_4FGLDR1_DR2_DR3.to_csv('../../CSV/joined_3FGL_4FGLDR1_DR2_DR3.csv')
+
+def flag_df(df):
+    if df['CLASS1-3FGL'] == "" or df['CLASS1-3FGL'] is None:
+        return 0
+    elif df['CLASS1-4FGLDR1'] == "" or df['CLASS1-4FGLDR1'] is None \
+            or df['CLASS1-4FGLDR2'] == "" or df['CLASS1-4FGLDR2'] is None \
+            or df['CLASS1-4FGLDR3'] == "" or df['CLASS1-4FGLDR3'] is None \
+            or df['CLASS1-4FGLDR4'] == "" or df['CLASS1-4FGLDR4'] is None:
+        return 1
+    elif ((df['CLASS1-3FGL'] == df['CLASS1-4FGLDR1']) \
+            and (df['CLASS1-3FGL'] == df['CLASS1-4FGLDR2']) \
+            and (df['CLASS1-3FGL'] == df['CLASS1-4FGLDR3']) \
+            and (df['CLASS1-3FGL'] == df['CLASS1-4FGLDR4'])):
+        return 2
+    elif ((df['CLASS1-3FGL'] != "") \
+          and (df['CLASS1-4FGLDR1'] != "") \
+          and (df['CLASS1-4FGLDR2'] != "") \
+          and (df['CLASS1-4FGLDR3'] != "") \
+          and (df['CLASS1-4FGLDR4'] != "")):
+        return 3
+    else:
+        return 4
+
+print("=========================Merge 3FGL, 4FGL-DR1, 4FGL-DR2, 4FGL-DR3, 4FGL-DR4=========================")
+joined_3FGL_4FGLDR1_DR2_DR3_DR4 = pd.merge(joined_3FGL_4FGLDR1_DR2_DR3, fgl4dr4_df, left_on='3FGL', right_on='4FGLDR4-3FGL', how='left')
+joined_3FGL_4FGLDR1_DR2_DR3_DR4['status'] = joined_3FGL_4FGLDR1_DR2_DR3_DR4.apply(flag_df, axis = 1)
+joined_3FGL_4FGLDR1_DR2_DR3_DR4.to_csv('../../CSV/joined_3FGL_4FGLDR1_DR2_DR3_DR4.csv')
